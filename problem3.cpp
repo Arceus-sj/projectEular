092437113293394e7fd3311c55e516fc68692f19
Arceus-sj <71997730+Arceus-sj@users.noreply.github.com>
2021-07-24 18:19:59 +0530

    optimized  solution

--- problem3.cpp
+++ problem3.cpp
@@ -1,30 +1,21 @@
 #include<iostream>
+#include<cmath>
 #define ll long long
 
 using namespace std;
 
-bool isPrime(int n) {
-    for(int i = 2; i <= n - 1; i++) {
-        if(n % i == 0) {
-            return false;
-        }
-    }
-
-    return true;    
-}
-
- 
 void largestPrimeFactor(ll int num) {
-    for(int i = 2; i <= num; i++) {
-        if(isPrime(i)) {
+    for(int i = 2; i <= sqrt(num); i++) {
+        
             if(num % i == 0) {
                 while(num % i == 0) {
                     num = num / i;
                 }
                 cout << i << " ";
             }
-        }
     }
+    if(num != 1)
+        cout << num << " " ;
 }
 
 int main() {
@@ -32,6 +23,7 @@ int main() {
     ll int number;
     cin >> number;
 
+     
     largestPrimeFactor(number);
     return 0;
 }
